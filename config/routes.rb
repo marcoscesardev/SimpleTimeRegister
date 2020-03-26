a6378ac84d1a37e743731dcb0398d349436c19df
marcos <mcsar1997@gmail.com>
2020-03-25 21:02:04 -0300

    feat: Add stack template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :employees
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: 'stack#index'
+
+  get ':page' => 'stack#show', as: 'stack'
 end
