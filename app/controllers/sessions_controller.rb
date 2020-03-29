8c6b38d177a13f21446bc99e0d612e714d8792f5
marcos <mcsar1997@gmail.com>
2020-03-29 17:05:18 -0300

    feat: Remove code stack template and rails unused

--- app/controllers/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class SessionsController < Devise::SessionsController
-  layout :layout_name
-
-  def layout_name
-    return 'application' if action_name.in?(%w[edit update])
-
-    'auth'
-  end
-end
